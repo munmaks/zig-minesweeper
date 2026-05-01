ef8520ef4e320da3e2617282cb2a19623434bf84
munmaks <maksmunait@gmail.com>
2026-05-01 17:47:13 +0200

    #2 add raylib

--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,65 @@
 const std = @import("std");
 const Io = std.Io;
+const rl = @import("raylib");
 
 const zig_minesweeper = @import("zig_minesweeper");
 
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    _ = init;
+    // // Prints to stderr, unbuffered, ignoring potential errors.
+    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
+    // // This is appropriate for anything that lives as long as the process.
+    // const arena: std.mem.Allocator = init.arena.allocator();
 
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
+    // // Accessing command line arguments:
+    // const args = try init.minimal.args.toSlice(arena);
+    // for (args) |arg| {
+    //     std.log.info("arg: {s}", .{arg});
+    // }
+
+    // // In order to do I/O operations need an `Io` instance.
+    // const io = init.io;
+
+    // // Stdout is for the actual output of your application, for example if you
+    // // are implementing gzip, then only the compressed bytes should be sent to
+    // // stdout, not any debugging messages.
+    // var stdout_buffer: [1024]u8 = undefined;
+    // var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    // const stdout_writer = &stdout_file_writer.interface;
+
+    // try zig_minesweeper.printAnotherMessage(stdout_writer);
+
+    // try stdout_writer.flush(); // Don't forget to flush!
+
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    defer rl.closeWindow(); // Close window and OpenGL context
+
+    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(.white);
+
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
+        //----------------------------------------------------------------------------------
     }
-
-    // In order to do I/O operations need an `Io` instance.
-    const io = init.io;
-
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-
-    try zig_minesweeper.printAnotherMessage(stdout_writer);
-
-    try stdout_writer.flush(); // Don't forget to flush!
 }
 
 test "simple test" {
